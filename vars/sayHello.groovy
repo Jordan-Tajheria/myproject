a5decb7d692f6246d2d288ca509f47ce355e514c
Jordan-Tajheria <jordan.roberts@publicissapient.com>
2022-06-22 13:38:20 +0100

    Adding function to groovy file to create and install helm

--- vars/sayHello.groovy
+++ vars/sayHello.groovy
@@ -3,3 +3,9 @@
 def call(String name = 'human') {
     echo "Hello, ${name}."
 }
+
+def helmCreate(String helmchartName, String releaseName) {
+    sh "helm create ${helmchartName}"
+    sh "ls ${helmchartName}"
+    sh "helm install ${releaseName} ${helmchartName}"
+}
