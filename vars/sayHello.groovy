5cf9856b6fa57105ba2f9b6c9a6a2aa096f8b197
Jordan-Tajheria <jordan.roberts@publicissapient.com>
2022-06-23 11:49:21 +0100

    Changing line command

--- vars/sayHello.groovy
+++ vars/sayHello.groovy
@@ -1,18 +1,5 @@
 #!/usr/bin/env groovy
 
-/*
-def helloWorld(String name) {
-    echo "Hello, ${name}."
-}
-
-
-def call(String helmchartName, String releaseName) {
-    sh "helm create ${helmchartName}"
-    sh "ls ${helmchartName}"
-    sh "helm install ${releaseName} ${helmchartName}"
-}
-*/
-
 def call(){
     echo "I REALLY, REALLY, REALLY NEED THIS TO WORK. :D"
 }
