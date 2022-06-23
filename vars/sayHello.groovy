7144e605c6d003a8ce68604645ac4b4f8f6c8eaf
Jordan-Tajheria <jordan.roberts@publicissapient.com>
2022-06-23 11:30:50 +0100

    test'

--- vars/sayHello.groovy
+++ vars/sayHello.groovy
@@ -4,10 +4,15 @@
 def helloWorld(String name) {
     echo "Hello, ${name}."
 }
-*/
+
 
 def call(String helmchartName, String releaseName) {
     sh "helm create ${helmchartName}"
     sh "ls ${helmchartName}"
     sh "helm install ${releaseName} ${helmchartName}"
+}
+*/
+
+def call(){
+    sh "echo I REALLY, REALLY, REALLY NEED THIS TO WORK."
 }
